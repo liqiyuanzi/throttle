f235679344e002eb7201e22246afd5463854213f
李起源 <liqiyuan@zuoshouyisheng.com>
2020-01-21 11:16:46 +0800

    clear

--- throttle.js
+++ throttle.js
@@ -4,26 +4,32 @@ function throttle(fn,delay,options){
 	var previous = 0;
 	options = options || {};
 	
+	var now = function(){
+		return +new Date
+	}
 	var throttled = function(){
 		context = this;
 		args = arguments;
-		var now = +new Date;
-		
-		if(!previous && options.leading === false) previous = now;
-		var remaing = delay - (now - previous);
+		var _now = now()
+		if(!previous && options.leading === false) previous = _now;
+		var remaing = delay - (_now - previous);
 		if(remaing <= 0){
-			previous = now;
+			previous = _now;
 			result = fn.apply(context,args);
 		}else{
 			if(!timer && options.trailing !== false)
 				timer =setTimeout(function(){
-					previous = options.leading === false ? 0 : +new Date;
+					previous = options.leading === false ? 0 : now();
 					timer = null;
 					result = fn.call(context,args)
 				},remaing)
 		}
 		return result
-
+	}
+	throttled.clear = function(){
+		clearTimeout(timer)
+		previous = 0;
+		timer = null;
 	}
 	return throttled
 }
